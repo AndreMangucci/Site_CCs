c8186c9d1b823a31e3fd73247ce99c17a35ed6e6
Kaique Tomazini Alves Pereira <46302870+KaiqueTomazini@users.noreply.github.com>
2020-11-28 18:09:56 -0300

    Update functions_minhaconta.js
    Foi colocado o atributo display em vez do visibility, pois estava utilizando o position absolute e o mesmo estava fazendo o menu ficar embaixo

--- js/functions_minhaconta.js
+++ js/functions_minhaconta.js
@@ -1,27 +1,27 @@
 function showItem(x){
     if(x == 1){
-        document.getElementById("dados").style.visibility = "visible";
-        document.getElementById("pedidos").style.visibility = "hidden";
-        document.getElementById("desejos").style.visibility = "hidden";
-        document.getElementById("enderecos").style.visibility = "hidden";
+        document.getElementById("dados").style.display = "block";
+        document.getElementById("pedidos").style.display = "none";
+        document.getElementById("desejos").style.display = "none";
+        document.getElementById("enderecos").style.display = "none";
     }
     else if(x == 2){
-        document.getElementById("dados").style.visibility = "hidden";
-        document.getElementById("pedidos").style.visibility = "visible";
-        document.getElementById("desejos").style.visibility = "hidden";
-        document.getElementById("enderecos").style.visibility = "hidden";
+        document.getElementById("dados").style.display = "none";
+        document.getElementById("pedidos").style.display = "block";
+        document.getElementById("desejos").style.display = "none";
+        document.getElementById("enderecos").style.display = "none";
     }
     else if(x == 3){
-        document.getElementById("dados").style.visibility = "hidden";
-        document.getElementById("pedidos").style.visibility = "hidden";
-        document.getElementById("desejos").style.visibility = "visible";
-        document.getElementById("enderecos").style.visibility = "hidden";
+        document.getElementById("dados").style.display = "none";
+        document.getElementById("pedidos").style.display = "none";
+        document.getElementById("desejos").style.display = "block";
+        document.getElementById("enderecos").style.display = "none";
     }
     else if(x == 4){
-        document.getElementById("dados").style.visibility = "hidden";
-        document.getElementById("pedidos").style.visibility = "hidden";
-        document.getElementById("desejos").style.visibility = "hidden";
-        document.getElementById("enderecos").style.visibility = "visible";
+        document.getElementById("dados").style.display = "none";
+        document.getElementById("pedidos").style.display = "none";
+        document.getElementById("desejos").style.display = "none";
+        document.getElementById("enderecos").style.display = "block";
     }
     else {
         alert("Não foi possivel reconhecer o botao")
