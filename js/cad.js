6d4503b1046a9dc75ca3100f3ff26efc3b2ba1f3
Kaique Tomazini Alves Pereira <46302870+KaiqueTomazini@users.noreply.github.com>
2020-11-28 19:42:13 -0300

    Update cad.js

--- js/cad.js
+++ js/cad.js
@@ -1,33 +1,33 @@
 function cadastroDados() {
-var nomex  = document.formulario.nome.value
-var enderx = document.formulario.ender.value
-var mailx  = document.formulario.mail
-var cpfx   = document.formulario.cpf.value
-var senhax = document.formulario.senha.value
+var nomex  = document.formulario.nome.value;
+var enderx = document.formulario.ender.value;
+var mailx  = document.formulario.mail;
+var cpfx   = document.formulario.cpf.value;
+var senhax = document.formulario.senha.value;
 if (nomex=="") {
-		alert("invalido")
-		nomex.focus()
-		return false
+		alert("invalido");
+		nomex.focus();
+		return false;
 
 	}if (enderx==""){
-		alert("invalido endereco")
-		enderx.focus()
-		return false
+		alert("invalido endereco");
+		enderx.focus();
+		return false;
 	}
 	if (mailx=="" || mailx.indexOf("@")== -1 || mailx.indexOf(".")== -1){
-		alert ("invalido email")
-		mailx.focus()
-		return false
+		alert ("invalido email");
+		mailx.focus();
+		return false;
 	}
 	if (cpfx=="" || cpfx.length < 11 ||  cpfx.length > 11 ) {
-		alert ("cpf invalido")
-		cpfx.focus()
-		return false
+		alert ("cpf invalido");
+		cpfx.focus();
+		return false;
 	}
 
 	if (senhax != 12) {
-		alert ("senha invalida")
-		senhax.focus()
-		return false
+		alert ("senha invalida");
+		senhax.focus();
+		return false;
 	}
-	}
+}
